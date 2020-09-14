60e819e26cfd4d6953c20fcebd92dc2043738325
Ahmet Sevim <a_sevim@icloud.com>
2020-09-14 21:23:09 +0300

    Update main.cpp
    dev

--- main.cpp
+++ main.cpp
@@ -3,12 +3,19 @@
 //#include "QMC5883L.h"
 #include "controller.h"
 #include "PID.h"
-#include "MS5837.h"
+//#include "MS5837.h"
 
-#define Kc 2.96
-#define Ti 0.01
-#define Td 0.5
-#define RATE 0.1
+#define PID_ROLL_Kc 2.16
+#define PID_ROLL_Ti 1.75
+#define PID_ROLL_Td 0.01
+
+#define PID_PITCH_Kc 2.16
+#define PID_PITCH_Ti 1.75
+#define PID_PITCH_Td 0.01
+
+#define PID_YAW_Kc 4
+#define PID_YAW_Ti 1.5
+#define PID_YAW_Td 0
 
 #define PID_ROLL_PITCH_IN 90
 
@@ -33,12 +40,13 @@ FileHandle *mbed::mbed_override_console(int fd)
 
 MPU6050 mpu(PB_7,PB_6);
 //QMC5883L qmc(PB_7,PB_6);
-MS5837 ms(PB_7,PB_6);
+//MS5837 ms(PB_7,PB_6);
 
 Timer timer;
 
 DigitalOut led1(PB_0);
 DigitalOut led2(PB_2);
+AnalogIn a0(PA_0);
 
 PwmOut mOnSag(PB_15);
 PwmOut mOnSol(PB_14);
@@ -48,9 +56,9 @@ PwmOut mUstArka(PA_4);
 PwmOut mUstSag(PC_9);
 PwmOut mUstSol(PC_8);
 
-PID pitchPID (Kc, Ti, Td, RATE);
-PID yawPID (Kc, Ti, Td, RATE);
-PID rollPID (Kc, Ti, Td, RATE);
+PID pitchPID (PID_PITCH_Kc, PID_PITCH_Ti, PID_PITCH_Td, 0.1);
+PID rollPID (PID_ROLL_Kc, PID_ROLL_Ti, PID_ROLL_Td, 0.1);
+PID yawPID (PID_YAW_Kc, PID_YAW_Ti, PID_YAW_Td, 0.1);
 
 CAN can1(PB_8, PB_9, 500000);
 Controller controller;
@@ -65,7 +73,7 @@ float timeDiff;
 
 float accOffset[3];
 float gyroOffset[3];
-float angle[3]; 
+float angle[3];
 
 bool isRobotActive = false;
 void checkIsRobotActive() {
@@ -91,11 +99,9 @@ void checkAutonomous(bool robotActive) {
     }
 
     if(autonomousMod) {
-        led1 = true;
-        led2 = false;
-    }else {
-        led2 = true;
         led1 = false;
+    }else {
+        led1 = true;
     }
 }
 
@@ -106,7 +112,6 @@ void AutonomousDrive(int pitchDiff, int yawDiff, int rollDiff) {
 int byteToMotorValue(int val) {
     return val*4+(255/1000)+1000;
 }
-
 int fitMotorValue(int val, bool reverse = false){
     int result = val;
     
@@ -123,37 +128,39 @@ int fitMotorValue(int val, bool reverse = false){
         result = 3000 - result;
     }
     return result;
-}
+} 
+
 bool isAnalogStickMoved(int stick){
     return fitMotorValue(byteToMotorValue(stick)) != 1500;
 }
-
+ 
 void JoystickCheck(int yawAngle, int pitchAngle, int rollAngle){
     if(isAnalogStickMoved(controller.leftX)){
         yawPID.setSetPoint(yawAngle);
     }  
-    int trigger = (controller.leftTrigger - controller.rightTrigger)/6;
-    pitchPID.setSetPoint(trigger);
+    // int trigger = (controller.leftTrigger - controller.rightTrigger)/6;
+    // pitchPID.setSetPoint(trigger);
 }
 
 void ManuelDrive(int pitchDiff, int yawDiff, int rollDiff) {
-    int mOnsag = (1500 - (fitMotorValue(yawDiff) - 1500) - (byteToMotorValue(controller.rightY) - 1500) + (byteToMotorValue(controller.rightX) - 1500)  + (byteToMotorValue(controller.leftX) - 1500));
-    int mOnsol = (1500 - (fitMotorValue(yawDiff,true) - 1500) - (byteToMotorValue(controller.rightY) - 1500) - (byteToMotorValue(controller.rightX) - 1500)  - (byteToMotorValue(controller.leftX) - 1500));
-    int mArkasag = (1500 + (fitMotorValue(yawDiff,true) - 1500) - (byteToMotorValue(controller.rightY) - 1500) - (byteToMotorValue(controller.rightX) - 1500)  + (byteToMotorValue(controller.leftX) - 1500));
-    int mArkasol = (1500 - (fitMotorValue(yawDiff,true) - 1500) - (byteToMotorValue(controller.rightY) - 1500) - (byteToMotorValue(controller.rightX) - 1500)  + (byteToMotorValue(controller.leftX) - 1500));
+    int mOnsag = (1500 - (yawDiff - 1500) - (byteToMotorValue(controller.rightY) - 1500) + (byteToMotorValue(controller.rightX) - 1500)  + (byteToMotorValue(controller.leftX) - 1500));
+    int mOnsol = (1500 - (yawDiff - 1500) - (byteToMotorValue(controller.rightY) - 1500) - (byteToMotorValue(controller.rightX) - 1500)  - (byteToMotorValue(controller.leftX) - 1500));
+    int mArkasag = (1500 - (yawDiff - 1500) + (byteToMotorValue(controller.rightY) - 1500) + (byteToMotorValue(controller.rightX) - 1500)  - (byteToMotorValue(controller.leftX) - 1500));
+    int mArkasol = (1500 - (yawDiff - 1500) - (byteToMotorValue(controller.rightY) - 1500) + (byteToMotorValue(controller.rightX) - 1500)  - (byteToMotorValue(controller.leftX) - 1500));
+
+    int mUstarka = (1500 +  (byteToMotorValue(controller.leftY) - 1500));
+    int mUstsag = (1500 + (byteToMotorValue(controller.leftY) - 1500));
+    int mUstsol = (1500 - (byteToMotorValue(controller.leftY) - 1500));
 
     mOnSag.pulsewidth_us(fitMotorValue(mOnsag));
     mOnSol.pulsewidth_us(fitMotorValue(mOnsol));
-    mArkaSag.pulsewidth_us(fitMotorValue(mArkasag,true));
+    mArkaSag.pulsewidth_us(fitMotorValue(mArkasag));
     mArkaSol.pulsewidth_us(fitMotorValue(mArkasol));
 
-    int mUstarka = (1500 + (fitMotorValue(pitchDiff,true) - 1500) + (byteToMotorValue(controller.leftY) - 1500));
-    int mUstsag = (1500 + (fitMotorValue(rollDiff) - 1500) + (fitMotorValue(pitchDiff) - 1500) + (byteToMotorValue(controller.leftY) - 1500));
-    int mUstsol = (1500 + (fitMotorValue(rollDiff,true) - 1500) +  (fitMotorValue(pitchDiff) - 1500) + (byteToMotorValue(controller.leftY) - 1500));
-
-    mUstArka.pulsewidth_us(fitMotorValue(mUstarka,true));
-    mUstSag.pulsewidth_us(fitMotorValue(mUstsag, true));
+    mUstArka.pulsewidth_us(fitMotorValue(mUstarka));
+    mUstSag.pulsewidth_us(fitMotorValue(mUstsag));
     mUstSol.pulsewidth_us(fitMotorValue(mUstsol));
+
 }
 
 void initMotors() {
@@ -205,6 +212,9 @@ void resetRobot() {
     pitchPID.setSetPoint(0);
     yawPID.setSetPoint(0);
     rollPID.setSetPoint(0);
+    pitchPID.reset();
+    yawPID.reset();
+    rollPID.reset();
     initGyro(accOffset, gyroOffset);
     mOnSag.pulsewidth_us(1500);
     mOnSol.pulsewidth_us(1500);
@@ -213,7 +223,6 @@ void resetRobot() {
     mUstArka.pulsewidth_us(1500);
     mUstSag.pulsewidth_us(1500);
     mUstSol.pulsewidth_us(1500);
-
 }
 
 int main()
@@ -226,23 +235,36 @@ int main()
     pitchPID.setSetPoint(0);
     yawPID.setSetPoint(0);
     rollPID.setSetPoint(0);
-    ms.MS5837Init();
-    
-   
 
     timer.start();
-    prevTime = chrono::duration<float>(timer.elapsed_time()).count();  
+    prevTime = chrono::duration<float>(timer.elapsed_time()).count();
 
     while (true) {
+        int batteryVoltage = (10000+1000) / 1000 * (3.3 / 65535) * a0.read_u16() *10;
+        led2 = batteryVoltage > 180;
+        //printf("analog: %d\n",batteryVoltage); 
+        
         currTime = chrono::duration<float>(timer.elapsed_time()).count();
         timeDiff = currTime - prevTime;
+        prevTime = currTime;
+
+        if (can1.read(msg)) {
+            controller.SetKeyValues(msg.id, msg.data);
+            checkIsRobotActive();
+            checkAutonomous(isRobotActive);
+        }
+
+        if(!isRobotActive) {
+            resetRobot();
+            led1 = !led1;
+            continue;
+        }
+
         mpu.computeAngle (angle, accOffset, gyroOffset, timeDiff);
 
         yawPID.setInterval(timeDiff);
         pitchPID.setInterval(timeDiff);
         rollPID.setInterval(timeDiff);
-
-        prevTime = chrono::duration<float>(timer.elapsed_time()).count();
 
         int yawAngle = (int)angle[MPU_Z_AXIS] % PID_YAW_IN;
         int pitchAngle = (int)angle[MPU_Y_AXIS] > PID_ROLL_PITCH_IN ? PID_ROLL_PITCH_IN : angle[MPU_Y_AXIS];
@@ -254,38 +276,17 @@ int main()
         pitchPID.setProcessValue (pitchAngle);
         rollPID.setProcessValue (rollAngle);
 
+
         pitchDiff = pitchPID.compute();
         yawDiff = yawPID.compute();
         rollDiff = rollPID.compute();
 
-        int pressure;
-        int temperature;
-        ms.Barometer_MS5837();
-        pressure = (int)ms.MS5837_Pressure();
-        temperature = (int)ms.MS5837_Temperature();
-
-        //printf("press: %d\n",pressure);   
-        //printf("temp: %d\n",temperature);      
-
-        if (can1.read(msg)) {
-            controller.SetKeyValues(msg.id, msg.data);
-            checkIsRobotActive();
-            checkAutonomous(isRobotActive);
+        if(autonomousMod) {
+            AutonomousDrive((int)pitchDiff, (int)yawDiff, (int)rollDiff);
         }
-        if(!isRobotActive) {
-            resetRobot();
-            bool ledVal = !led1;
-            led1 = ledVal;
-            led2 = ledVal;
-            wait_us(50000);
-        }else{
-            if(autonomousMod) {
-                AutonomousDrive((int)pitchDiff, (int)yawDiff, (int)rollDiff);
-            }
-            else {
-                ManuelDrive((int)pitchDiff, (int)yawDiff, (int)rollDiff);
-                JoystickCheck(yawAngle, pitchAngle, rollAngle);
-            }
+        else {
+            ManuelDrive((int)pitchDiff, (int)yawDiff, (int)rollDiff);
+            JoystickCheck(yawAngle, pitchAngle, rollAngle);
         }
     }
 }
